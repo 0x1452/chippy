ef129e7f0f235aae854b7d4af9a0ac44bab2506c
0x1452 <56117258+0x1452@users.noreply.github.com>
2020-10-11 20:40:17 +0200

    Use std::time::Instant to handle timing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 extern crate minifb;
 
 use chippy::chip8::Chip8;
-use chippy::display::Display;
-use io::Read;
-use std::io;
-use std::time::Duration;
-use std::{error, fs, path::Path};
+use std::{error, path::Path};
+use std::time::Instant;
 
 use minifb::{Key, Scale, Window, WindowOptions};
 
 fn main() -> Result<(), Box<dyn error::Error>> {
-    let mut chip8 = Chip8::new(&Path::new("roms/TETRIS"))?;
+    let mut chip8 = Chip8::new(&Path::new("roms/BRIX"))?;
 
     let mut options = WindowOptions::default();
     options.scale = Scale::X8;
@@ -19,28 +16,31 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         panic!("{}", e);
     });
 
-    // 60hz
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16667)));
-    // slow lol
-    //window.limit_update_rate(Some(std::time::Duration::from_millis(500)));
-
+    let mut now = Instant::now();
     while window.is_open() && !window.is_key_down(Key::Escape) {
+        chip8.keypad.update_keypad(window.get_keys());
+
+        // TODO: most games seem to require different timing
+        //       should be adjustable by the user and/or provide default values for known games
+        if now.elapsed().as_micros() <= 1428*2 {
+            continue;
+        }
+
         if !chip8.done() { 
             chip8.run();
         }
-
-        chip8.keypad.update_keypad(window.get_keys());
 
         if chip8.display.is_dirty {
             window
                 .update_with_buffer(&chip8.display.screen, 64, 32)
                 .unwrap();
-            
+
             chip8.display.is_dirty = false;
         }
+        
         //chip8.debug_print();
 
-        //io::stdin().bytes().next();
+        now = Instant::now();
     }
 
     Ok(())
